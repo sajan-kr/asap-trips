0953dc6d7a9e517380526b3cc1c8a4d499ae9adc
sajan-kr <yourgmail@gmail.com>
2026-05-27 22:40:12 +0530

    saparate file

---
+++ js/main.js
@@ -0,0 +1,39 @@
+// =========================
+// LOAD HEADER
+// =========================
+
+fetch("components/header.html")
+  .then(response => response.text())
+  .then(data => {
+    document.getElementById("header").innerHTML = data;
+  });
+
+// =========================
+// LOAD FOOTER
+// =========================
+
+fetch("components/footer.html")
+  .then(response => response.text())
+  .then(data => {
+    document.getElementById("footer").innerHTML = data;
+  });
+
+// =========================
+// NAVBAR SCROLL EFFECT
+// =========================
+
+window.addEventListener("scroll", function () {
+
+  const navbar =
+    document.querySelector(".custom-navbar");
+
+  if (navbar) {
+
+    navbar.classList.toggle(
+      "scrolled",
+      window.scrollY > 50
+    );
+
+  }
+
+});
